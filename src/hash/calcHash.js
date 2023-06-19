9d38f6a9f5c062a0083f3712130a926583651e26
Artem Mokhov <teumik@gmail.com>
2023-06-19 13:58:14 +0400

    feat: implement calcHash function

--- src/hash/calcHash.js
+++ src/hash/calcHash.js
@@ -1,5 +1,19 @@
+import { fileURLToPath } from 'node:url';
+import { dirname, resolve } from 'node:path';
+import { readFile } from 'node:fs/promises';
+import { createHash } from 'node:crypto';
+
 const calculateHash = async () => {
-    // Write your code here 
+  const __filename = fileURLToPath(import.meta.url);
+  const __dirname = dirname(__filename);
+
+  const folder = resolve(__dirname, 'files');
+  const file = resolve(folder, 'fileToCalculateHashFor.txt');
+
+  const content = await readFile(file, { encoding: 'utf8' });
+
+  const res = createHash('sha256').update(content).digest('hex');
+  console.log(res);
 };
 
 await calculateHash();
