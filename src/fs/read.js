a0537e7fd84015b546be79c3f26089e47734e936
Artem Mokhov <teumik@gmail.com>
2023-06-19 11:47:17 +0400

    feat: implement read function

--- src/fs/read.js
+++ src/fs/read.js
@@ -1,5 +1,22 @@
+import { readFile } from 'node:fs/promises';
+import { dirname, resolve } from 'node:path';
+import { fileURLToPath } from 'node:url';
+
 const read = async () => {
-    // Write your code here 
+  const __filename = fileURLToPath(import.meta.url);
+  const __dirname = dirname(__filename);
+
+  const message = 'FS operation failed';
+
+  try {
+    const folder = resolve(__dirname, 'files');
+    const file = resolve(folder, 'fileToRead.txt');
+
+    const content = await readFile(file, { encoding: 'utf8' });
+    console.log(content);
+  } catch {
+    console.error(message);
+  }
 };
 
 await read();
