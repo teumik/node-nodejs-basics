4b7de040120a29d76cbd65c1e593d895e777e39c
Artem Mokhov <teumik@gmail.com>
2023-06-19 11:05:08 +0400

    feat: implement create function

--- src/fs/create.js
+++ src/fs/create.js
@@ -1,5 +1,27 @@
+import { readFile, writeFile } from 'fs/promises';
+import { dirname, resolve } from 'path';
+import { fileURLToPath } from 'url';
+
 const create = async () => {
-    // Write your code here 
+  const __filename = fileURLToPath(import.meta.url);
+  const __dirname = dirname(__filename);
+
+  const content = 'I am fresh and young';
+  const message = 'FS operation failed';
+  const file = 'fresh.txt';
+
+  try {
+    const path = resolve(__dirname, 'files', file);
+    await readFile(path);
+    console.error(message);
+  } catch {
+    try {
+      const path = resolve(__dirname, 'files', file);
+      await writeFile(path, content);
+    } catch (error) {
+      console.error(error);
+    }
+  }
 };
 
 await create();
