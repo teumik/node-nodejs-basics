27ae2015909e5bd7f87ae01a7e1b2a3e805f4d65
Artem Mokhov <teumik@gmail.com>
2023-06-21 22:29:45 +0400

    feat: implement compress function

--- src/zip/compress.js
+++ src/zip/compress.js
@@ -1,5 +1,29 @@
+import { createReadStream, createWriteStream, existsSync } from 'node:fs';
+import { dirname, resolve } from 'node:path';
+import { exit } from 'node:process';
+import { fileURLToPath } from 'node:url';
+import { createGzip } from 'node:zlib';
+
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = dirname(__filename);
+
 const compress = async () => {
-    // Write your code here 
+  const folder = resolve(__dirname, 'files');
+  const file = resolve(folder, 'fileToCompress.txt');
+  const archive = resolve(folder, 'archive.gz');
+  const message = 'File does not exist';
+
+  if (!existsSync(file)) {
+    console.error(message);
+    exit();
+  };
+
+  const gzip = createGzip();
+
+  const reader = createReadStream(file);
+  const writer = createWriteStream(archive);
+
+  reader.pipe(gzip).pipe(writer);
 };
 
 await compress();
