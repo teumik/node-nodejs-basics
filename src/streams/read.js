31eb7269c76ae39c46e331bb702c8558f80ea66e
Artem Mokhov <teumik@gmail.com>
2023-06-21 22:29:14 +0400

    feat: implement read function

--- src/streams/read.js
+++ src/streams/read.js
@@ -1,5 +1,21 @@
+import { createReadStream } from 'node:fs';
+import { EOL } from 'node:os';
+import { dirname, resolve } from 'node:path';
+import { stdout } from 'node:process';
+import { fileURLToPath } from 'node:url';
+
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = dirname(__filename);
+
 const read = async () => {
-    // Write your code here 
+  const folder = resolve(__dirname, 'files');
+  const file = resolve(folder, 'fileToRead.txt');
+
+  const reader = createReadStream(file);
+  reader.pipe(stdout);
+  reader.on('close', () => {
+    stdout.write(EOL);
+  });
 };
 
 await read();
