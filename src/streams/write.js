09a2c593cf02d00b24fe7b3824affbff961b8077
Artem Mokhov <teumik@gmail.com>
2023-06-21 22:29:23 +0400

    feat: implement write function

--- src/streams/write.js
+++ src/streams/write.js
@@ -1,5 +1,22 @@
+import { createWriteStream } from 'node:fs';
+import { EOL } from 'node:os';
+import { dirname, resolve } from 'node:path';
+import { stdin, stdout } from 'node:process';
+import { fileURLToPath } from 'node:url';
+
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = dirname(__filename);
+
 const write = async () => {
-    // Write your code here 
+  const folder = resolve(__dirname, 'files');
+  const file = resolve(folder, 'fileToWrite.txt');
+
+  const writer = createWriteStream(file);
+
+  stdout.write(`Press enter to save text:${EOL}`);
+
+  stdin.pipe(writer);
+  stdin.resume();
 };
 
 await write();
