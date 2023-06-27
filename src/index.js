bc11c76d66b1e6994ec42c4ed588b5ae0c6b36e7
Artem Mokhov <teumik@gmail.com>
2023-06-27 15:16:49 +0400

    feat: add commands functional

--- src/index.js
+++ src/index.js
@@ -1,29 +1,47 @@
-import os from 'os';
-
-const capitalize = (str) => {
-  return str.charAt(0).toUpperCase() + str.slice(1);
-};
-
-const parseUsername = () => {
-  const [arg] = process.argv.slice(2);
-  const [, username] = arg.split('=');
-  const parsedUsername = username.split('_').reduce((acc, word, i, arr) => {
-    acc += capitalize(word);
-    if (i !== arr.length - 1) acc += ' ';
-    return acc;
-  }, '');
-
-  return parsedUsername;
-}
+import { stdin } from 'process';
+import { parseUsername } from './helpers/parseUsername.js';
+import { print } from './helpers/print.js';
+import { up } from './commands/up.js';
+import { onExit } from './helpers/onExit.js';
+import { ls } from './commands/ls.js';
+import { cd } from './commands/cd.js';
+import { sp } from './commands/sp.js';
 
 const start = () => {
   const parsedUsername = parseUsername();
   const welcomeMessage = `Welcome to the File Manager, ${parsedUsername}!`;
   const goodbyeMessage = `Thank you for using File Manager, ${parsedUsername}, goodbye!`;
-  const getDirectoryPath = (path) => `You are currently in ${path}`;
-  const homeDir = os.homedir();
+  // const getDirectoryPath = (path) => `You are currently in ${path}`;
 
-  console.log(homeDir);
+  print(welcomeMessage);
+  onExit(goodbyeMessage);
+
+  stdin.on('data', (data) => {
+    const [command, ...path] = data.toString().trim().split(' ');
+
+    switch (command) {
+      case 'sp': {
+        sp();
+        break;
+      }
+      case 'up': {
+        up();
+        break;
+      }
+      case 'cd': {
+        cd(path);
+        break;
+      }
+      case 'ls': {
+        ls();
+        break;
+      }
+      default: {
+        print('Invalid input');
+        break;
+      }
+    }
+  });
 };
 
 start();
