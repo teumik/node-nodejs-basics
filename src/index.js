578be064354d0c3b504a2366cf7a65e58089a42b
Artem Mokhov <teumik@gmail.com>
2023-07-01 15:10:35 +0400

    feat: add commands functional

--- src/index.js
+++ src/index.js
@@ -6,12 +6,16 @@ import { onExit } from './helpers/onExit.js';
 import { ls } from './commands/ls.js';
 import { cd } from './commands/cd.js';
 import { sp } from './commands/sp.js';
+import { cat } from './commands/cat.js';
+import { add } from './commands/add.js';
+import { rn } from './commands/rn.js';
+import { parsePaths } from './helpers/parsePaths.js';
 
 const start = () => {
   const parsedUsername = parseUsername();
   const welcomeMessage = `Welcome to the File Manager, ${parsedUsername}!`;
   const goodbyeMessage = `Thank you for using File Manager, ${parsedUsername}, goodbye!`;
-  // const getDirectoryPath = (path) => `You are currently in ${path}`;
+  const getDirectoryPath = (path) => `You are currently in ${path}`;
 
   print(welcomeMessage);
   onExit(goodbyeMessage);
@@ -23,23 +27,42 @@ const start = () => {
       case 'sp': {
         sp();
         break;
-      }
+      };
+
       case 'up': {
         up();
         break;
-      }
+      };
+
       case 'cd': {
-        cd(path);
+        cd(path.join(' '));
         break;
-      }
+      };
+
       case 'ls': {
         ls();
         break;
-      }
+      };
+
+      case 'cat': {
+        cat(path.join(' '));
+        break;
+      };
+
+      case 'add': {
+        add(path.join(' '));
+        break;
+      };
+
+      case 'rn': {
+        rn(parsePaths(path.join(' ')));
+        break;
+      };
+
       default: {
         print('Invalid input');
         break;
-      }
+      };
     }
   });
 };
